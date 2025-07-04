a7f6b6957cb048cbbea4b16bf09c9f03d43476fc
rickojn <rickojn@hotmail.com>
2025-07-04 13:52:14 +0100

    wip

--- ggml/src/ggml-cpu/ggml-cpu.c
+++ ggml/src/ggml-cpu/ggml-cpu.c
@@ -2851,12 +2851,13 @@ static thread_ret_t ggml_graph_compute_thread(void * data) {
     for (int node_n = 0; node_n < cgraph->n_nodes && atomic_load_explicit(&tp->abort, memory_order_relaxed) != node_n; node_n++) {
         struct ggml_tensor * node = cgraph->nodes[node_n];
 
-        
-        ggml_compute_forward(&params, node);
 
         if (strcmp(node->name, "node_22") == 0) {
             int db = 0;
         }
+
+        ggml_compute_forward(&params, node);
+
 
 
         //custard
